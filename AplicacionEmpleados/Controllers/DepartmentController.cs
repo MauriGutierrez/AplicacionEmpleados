1bcb24d88b51a03c54cb9cc471e5549f44695844
Mauricio Gutierrez <maurix554@gmail.com>
2021-09-27 12:05:04 -0300

    Add project files.

---
+++ AplicacionEmpleados/Controllers/DepartmentController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AplicacionEmpleados.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        } 
+       [HttpGet] 
+       public JsonResult Get()
+        {
+            string query = @"
+                   select DepartmentId, DepartmentName from 
+                    dbo.Department
+                    ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppController");
+            SqlDataReader myReader;
+            using(SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+
+            }
+            return new JsonResult(table);
+        }
+
+    }
+}
